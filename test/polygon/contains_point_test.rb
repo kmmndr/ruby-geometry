40a358257cbbe231308ed81a859c3fa2bfe6bcc5
Thomas Kienlen <kommander@laposte.net>
2015-05-27 11:26:38 +0200

    added test case for closed polygons

--- test/polygon/contains_point_test.rb
+++ test/polygon/contains_point_test.rb
@@ -70,4 +70,21 @@ class ContainsPointTest < MiniTest::Unit::TestCase
       assert ! nonconvex_polygon.contains?(outer_point)
     end
   end
+
+  def test_closed_polygon
+    coordinates_raw = "114.146011,22.284090 114.145050,22.282860 114.146240,22.279619 114.151176,22.277014 114.151833,22.276251 114.151443,22.275000 114.151550,22.273470 114.153198,22.272249 114.156143,22.272970 114.161690,22.271900 114.162521,22.274820 114.162697,22.276211 114.160957,22.275850 114.159927,22.276190 114.158340,22.276270 114.157082,22.276661 114.155312,22.276310 114.153732,22.278000 114.154312,22.278870 114.153992,22.279900 114.152321,22.280890 114.150261,22.282721 114.146011,22.284090"
+    coordinates = coordinates_raw.split(/\s+/).map do |raw_coor|
+      # latitude and longitude are reversed since they are from KML
+      # most important point : the last point is the same as the first one
+      raw_coor.split(',').map(&:to_f).reverse
+    end
+
+    vertices = coordinates.map do |coordinate_array|
+      Point.new_by_array(coordinate_array)
+    end
+    rectangle = Polygon.new(vertices)
+
+    inner = Point(22.2817, 114.14932)
+    assert rectangle.contains?(inner)
+  end
 end
